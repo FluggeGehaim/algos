b62109e87ad7ccef2af1902fa494e14297c20f3e
artem_babitski <temacaxp@gmail.com>
2025-07-23 16:50:54 +0300

    feat/binary_search

---
+++ constat.py
@@ -0,0 +1,7 @@
+from random import sample
+
+train_list_1000 = sample(range(1, 1001), 1000)
+train_list_751 = sample(range(1, 752), 751)
+train_list_500 = sample(range(1, 501), 500)
+train_list_321 = sample(range(1, 322), 321)
+train_list_99 = sample(range(1, 100), 99)
